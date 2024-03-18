6b4f3bf55187cda128c85a0a611930a87928b82f
Leopold Canou <leopold.canou@etu.unilim.fr>
2024-03-18 15:28:06 +0100

    Nouvelle version du projet

--- src/Controller/LegoController.php
+++ src/Controller/LegoController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Service\CreditsGenerator;
 use App\Repository\LegoRepository;
+use App\Entity\LegoCollection;
+use App\Repository\LegoCollectionRepository;
 
 /* le nom de la classe doit être cohérent avec le nom du fichier */
 class LegoController extends AbstractController
@@ -24,8 +26,16 @@ class LegoController extends AbstractController
     // L’attribute #[Route] indique ici que l'on associe la route
     // "/" à la méthode home pour que Symfony l'exécute chaque fois
     // que l'on accède à la racine de notre site.
-    #[Route('/')]
-    public function home(): Response
+    #[Route('/', name: 'home')]
+    public function home(LegoCollectionRepository $CollectionRepository): Response
+    {
+        $legos = $this->legoRepository->findAll();
+        $collections = $CollectionRepository->findAll();
+        return $this->render('lego.html.twig', ['legos' => $legos, 'collections' => $collections]);
+    }
+
+    #[Route('/lego', name: 'lego')]
+    public function lego(): Response
     {
 // use doctrine
         $legos = $this->legoRepository->findAll();
@@ -34,26 +44,36 @@ class LegoController extends AbstractController
     
 
 
-    #[Route('/{collection}', name : 'filter_by_collection', requirements: ['collection' => 'creator|star_wars|creator_expert'])]
-public function filter($collection): Response
-{
-$legos = $this->legoRepository->findAll();
-$collectionFormated = ucwords(str_replace('_', ' ', $collection));
+//     #[Route('/{collection}', name : 'filter_by_collection', requirements: ['collection' => 'creator|star_wars|creator_expert'])]
+// public function filter($collection): Response
+// {
+// $legos = $this->legoRepository->findAll();
+// $collectionFormated = ucwords(str_replace('_', ' ', $collection));
     
 
-$filteredLegos = array_filter($legos, function ($lego) use ($collectionFormated) {
-    return $lego->getCollection() === $collectionFormated;
-});
+// $filteredLegos = array_filter($legos, function ($lego) use ($collectionFormated) {
+//     return $lego->getCollection() === $collectionFormated;
+// });
 
-return $this->render('lego.html.twig', ['legos' => $filteredLegos, 'collection' => $collectionFormated]);
+// return $this->render('lego.html.twig', ['legos' => $filteredLegos, 'collection' => $collectionFormated]);
 
-}
+// }
 
 #[Route('/credits', 'credits')]
 public function credits(CreditsGenerator $credits): Response
 {
     return new Response($credits->generate());
 }
+
+#[Route('/{name}', name: 'collection')]
+public function collection(LegoCollection $collection,LegoCollectionRepository $CollectionRepository): Response
+{
+    $collections = $CollectionRepository->findAll();
+$legos =$collection->getLegos();    
+return $this->render('lego.html.twig', ['legos' => $legos, 'collections' => $collections]);
+}
+
+
 
 
 
